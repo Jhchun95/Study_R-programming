b38cc1675d9739a591fa622f95399ec99efbc2cf
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2022-02-12 23:51:20 +0900

    Update 0727_5.R

--- 0727_5.R
+++ 0727_5.R
@@ -12,9 +12,12 @@ n.diff2
 
 
 # [실습2] 자기회귀 누적이동평균 모델
+# 시계열 자료 : 시간에 따라 관측된 자료
+# 대부분의 많은 자료 시계열 자료가 자기회귀 누적이동평균 모델을 따름
 install.packages("forecast")      # forecast 패키지 설치하기
 library(forecast)                 # forecast 패키지 불러오기
 
+# ARIMA 모형은 기본적으로 비정상 시계열 모형 
 auto.arima(Nile)
 
 Nile.arima <- arima(Nile, order=c(1,1,1))
