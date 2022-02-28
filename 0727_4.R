ae5d1264b39d62634971a59c7fc90ffaddc1536d
Jhchun95 <75196751+Jhchun95@users.noreply.github.com>
2022-02-28 22:26:20 +0900

    Update 0727_4.R

--- 0727_4.R
+++ 0727_4.R
@@ -18,11 +18,13 @@ r <- residuals(model)
 r[0:4]
 
 # 잔차 제곱합
+# 로지스틱 회귀모형
+# deviance : 이탈도, 어떤 모형 M의 최대로그 우도(maximized log-likelihood) log(LM)에서 포화모형(saturated model) S의 최대로그우도 log(LS)를 뺀 것에 -2를 곱한 값
 deviance(model)
-
 
 # [실습3] 다중선형회귀|예측
 # 예측(점 추정)
+# 새로운 데이터를 input 시켜 새로운 데이터에 대한 예측된 값도 구할 수 있음
 predict.lm(model, newdata = data.frame(height_father = 170, height_mother = 160))
 # 예측(구간 추정)
 predict.lm(model, newdata = data.frame(height_father = 170, height_mother = 160),
